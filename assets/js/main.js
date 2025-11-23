6b32849ac4de27d49fc665481b77127aec824841
DDV2234 <167382412+DDV2234@users.noreply.github.com>
2025-11-23 15:41:09 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+/*
+	ZeroFour by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			offsetY: -22,
+			mode: 'fade',
+			noOpenerFade: true,
+			speed: 300,
+			detach: false
+		});
+
+	// Nav.
+
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+					'<span class="title">' + $('#logo').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+	// Back to Top button
+
+	// Create and append button
+	var $backToTop = $('<a href="#" id="back-to-top" aria-label="Back to top"><i class="fas fa-chevron-up"></i></a>');
+	$backToTop.appendTo($body);
+
+	// Click handler: smooth scroll
+	$backToTop.on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: 0 }, 600);
+	});
+
+	// Show/hide on scroll
+	$window.on('scroll', function() {
+		if ($window.scrollTop() > 300) {
+			$backToTop.addClass('show');
+		} else {
+			$backToTop.removeClass('show');
+		}
+	});
+
+})(jQuery);
